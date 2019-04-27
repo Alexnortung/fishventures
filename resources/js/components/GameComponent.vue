5d6ae3b6916acad5c143495e410f2028dd7ceb17
Alexander Nortung <alex_nortung@live.dk>
2019-04-27 18:37:01 +0200

    initial commit

---
+++ resources/js/components/GameComponent.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+    <div>
+        #game-wrapper
+            #clicker-area
+                #click-area-inner
+                    #clicker-object
+
+
+            #shop-area
+                <shop-header>Upgardes</shop-header>
+                
+                <shop-header>Shop</shop-header>
+                
+    </div>
+</template>
+
+<script>
+import ShopHeader from "./ShopHeader.vue";
+
+export default {
+    
+    data() {
+        money: 0
+    },
+
+    components: {
+        ShopHeader
+    }
+}
+</script>
+
+<style lang="scss">
+    
+    #shop-area {
+        
+    }
+</style>
---
+++ resources/js/components/ShopHeader.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+    <div>
+        .shop-header
+            span.shop-header-text 
+                <slot></slot>
+    </div>
+</template>
+
+<style lang="scss">
+    .shop-header {
+        width: 100%;
+        
+    }
+</style>
